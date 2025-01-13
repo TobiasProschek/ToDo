75bf885398cc17cec4002c8b1c006d2fd8104d97
kleinkop-mms <kleinkop@mediamarktsaturn.com>
2025-01-13 08:26:18 +0100

    Some changes to practice a Git rebase

--- src/main/kotlin/TaskRepoImpl.kt
+++ src/main/kotlin/TaskRepoImpl.kt
@@ -9,37 +9,34 @@ class TaskRepoImpl : TaskRepo {
     private var nextID = 1
 
     override fun add(title: String, text: String, priority: String, status: String): Task {
-        map[nextID] = Task(nextID, title, text, priority, status)
-        log.info {"Task wurde erfolgreich erstellt, ID = $nextID"}
+        val newTask = Task(nextID, title, text, priority, status)
+        map[nextID] = newTask
         nextID++
+        log.info {"New task created, ID = ${newTask.id}"}
         TODO("Return the created task")
     }
 
     override fun delete(task: Int): Boolean {
         if (map.containsKey(task)) {
             map.remove(task)
-            log.info { "Task mit der ID: $task wurde gelöscht" }
+            log.info { "Removed task with id $task" }
         } else {
-            log.info { "Task mit der ID: $task wurde nicht gefunden!" }
+            log.info { "Task with ID $task not found" }
         }
         TODO("Return the right boolean value")
     }
 
     override fun findById(taskId: Int): Task? {
         if (map.containsKey(taskId)) {
-            log.info { "Die Task mit der ID: $taskId ist: " }
-            log.info { "Title: ${map[taskId]?.title}" }
-            log.info { "Short_Text: ${map[taskId]?.shortText}" }
-            log.info { "Status: ${map[taskId]?.statusString}" }
-            log.info { "Priority: ${map[taskId]?.priorityString}" }
+            log.info { "Found task with id $taskId" }
         } else {
-            log.info { "Task mit der ID: $taskId konnte nicht gefunden werden!" }
+            log.info { "Task with ID $taskId not found" }
         }
         TODO("Return the created task")
     }
 
     override fun findByPriority(priority: String): List<Task> {
-        log.info { "Task mit der Priorität $priority werden gesucht." }
+        log.debug { "Searching for tasks with priority $priority..." }
 
         val filteredMap = map.filter { it.value.priorityString == priority }.keys
         TODO("Return list of matching tasks")
